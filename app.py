20da7c3c3f3113e3009a89d4f178996512744ffc
Rutendo230 <85679189+Rutendo230@users.noreply.github.com>
2021-06-10 12:34:57 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request
+import pandas as pd
+import model
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def hello():
+    if request.method == 'POST':
+        gender = request.form['gender']
+        SeniorCitizen = request.form['SeniorCitizen']
+        Partner = request.form['Partner']
+        Dependents = request.form['Dependents']
+        tenure = request.form['tenure']
+        PhoneService = request.form['PhoneService']
+        MultipleLines = request.form['MultipleLines']
+        InternetService = request.form['InternetService']
+        OnlineSecurity = request.form['OnlineSecurity']
+        OnlineBackup = request.form['OnlineBackup']
+        DeviceProtection = request.form['DeviceProtection']
+        TechSupport = request.form['TechSupport']
+        StreamingTV = request.form['StreamingTV']
+        StreamingMovies = request.form['StreamingMovies']
+        Contract = request.form['Contract']
+        PaperlessBilling = request.form['PaperlessBilling']
+        PaymentMethod = request.form['PaymentMethod']
+        MonthlyCharges = request.form['MonthlyCharges']
+        TotalCharges = request.form['TotalCharges']
+        
+        array1 = [gender, SeniorCitizen, Partner, Dependents, tenure, PhoneService, MultipleLines, InternetService,
+                  OnlineSecurity, OnlineBackup, DeviceProtection, TechSupport, StreamingTV, StreamingMovies, Contract,
+                  PaperlessBilling, PaymentMethod, MonthlyCharges, TotalCharges]
+        
+        array1 = pd.to_numeric(array1)
+        prediction = model.mp.predict([array1])
+        prediction = int(prediction)
+        if prediction == 0:
+            s='No'
+        else:
+            s='Yes'
+         
+    return render_template('index.html', value=s)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
